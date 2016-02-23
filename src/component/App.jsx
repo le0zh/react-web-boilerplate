91d9c5bd821cecf3a9224fc3dce1e059fbba46d6
le0zh <newlight@qq.com>
2016-02-23 16:30:50 +0800

    init

---
+++ src/component/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+//import DatePicker from 'antd/lib/date-picker'
+import {DatePicker, Button } from 'antd'
+
+// ES6 style
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state = {date: '2016-2-23'}
+	}
+
+	btnClickHandle(){
+		alert(this.state.date);
+	}
+
+	dateChange(val){
+		this.setState({
+			date: val
+		});
+	}
+
+	render() {
+		return (
+			<div style={{width:400, margin:'100px auto'}}>
+				<h3>hello world</h3>
+				<DatePicker onChange={(dateVal)=>this.dateChange(dateVal)}/>
+				
+				<div style={{marginTop:10}}>
+					<Button type="primary" onClick={()=>this.btnClickHandle()}>主按钮1</Button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App
