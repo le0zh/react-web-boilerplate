c2b61f7b6eb954da54c8c21fdfdb133d59c64e75
le0zh <newlight@qq.com>
2016-02-25 18:57:35 +0800

    fix menu click, add test content

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -3,6 +3,8 @@ import { Row, Col } from 'antd'
 import SiderMenu from './SiderMenu'
 import Topbar from './Topbar'
 
+import ContentDemo from './Test'
+
 import './layout.style.css'
 
 export default class App extends React.Component{
@@ -10,9 +12,16 @@ export default class App extends React.Component{
 		return (
 			<div>
 				<Topbar />
-				<SiderMenu styleName={'viewFramework-sidebar'} />
-				<div className={'content'}>
-					<p>main content</p>
+				<div className={'viewFramework-body'}>
+					<SiderMenu styleName={'viewFramework-sidebar'} />
+					<div className={'viewFramework-product'}>
+						<div className={'viewFramework-product-body'}>
+							<div className={'console-container'}>
+								<h2 style={{paddingTop: 15, paddingBottom: 15}}>测试内容</h2>
+								<ContentDemo />
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -1,57 +1,19 @@
 import React, {Component ,PropTypes} from 'react'
 import { Icon } from 'antd';
+import DropDownMenu from './TopBarDropDownMenu'
 
 export default class Topbar extends Component{
-	toggleDropdown(e){
-		//here event is native browser event
-		console.log('toggleDropdown', e);
-
-		//如果提供了事件对象，则这是一个非IE浏览器 
-		if ( e && e.stopPropagation ) 
-			//因此它支持W3C的stopPropagation()方法 
-			e.stopPropagation(); 
-		else{
-			//否则，我们需要使用IE的方式来取消事件冒泡 
-			window.event.cancelBubble = true; 
-		}
-		
-		var containerDom = this.refs.dropdownContainer;
-		if(containerDom){
-			if(containerDom.className.indexOf('open') >= 0){
-				//关闭
-				containerDom.className = containerDom.className.replace('open', '').trim();
-			}
-			else{
-				//打开
-				containerDom.className += " open";
-			}
-		}
+	constructor(){
+		super();
 	}
 
-	hideDropdown(e){
-		var containerDom = this.refs.dropdownContainer;
-		console.log('hideDropdown');
-
-		if(containerDom){
-			if(containerDom.className.indexOf('open') >= 0){
-				//关闭
-				containerDom.className = containerDom.className.replace('open', '').trim();
-			}
-		}
+	handleChangePassword(e){
+		alert('修改密码');
+		console.log('修改密码');
 	}
 
-	componentDidMount(){
-		console.log('componentDidMount');
-		var body = document;
-		console.log(body);
-		body.addEventListener('click', (e)=>this.hideDropdown(e));
-
-		var containerDom = this.refs.dropdownContainer;
-		//手动绑定点击事件, 否则没办法阻止事件冒泡
-		//参考: 
-		//https://medium.com/@ericclemmons/react-event-preventdefault-78c28c950e46#.fv2h9z238
-		//https://github.com/erikras/react-native-listener
-		containerDom.addEventListener('click', (e)=>this.toggleDropdown(e));
+	handleSignOut(e){
+		console.log('退出');
 	}
 
 	render(){
@@ -69,26 +31,21 @@ export default class Topbar extends Component{
 					<div className={'topbar-btn topbar-left border-left'}>
 						<Icon type="search" /> 搜索
 					</div>
-					<div className={'topbar-btn  topbar-left border-left'}>
+					<div className={'topbar-btn topbar-left border-left'}>
 						<Icon type="mobile" /> 手机版
 					</div>
-					<div className={'topbar-btn  topbar-left border-left'}>
-						<Icon type="question-circle-o" /> 帮助与文档
+					<div className={'topbar-btn topbar-left border-left'}>
+						<DropDownMenu title={<span><Icon type="question-circle-o" /> 帮助与文档</span>}>
+							<DropDownMenu.Item title={'帮助与文档'} url={'http://www.baidu.com'}/>
+							<DropDownMenu.Item title={'论坛'} url={'http://www.baidu.com'} />
+							<DropDownMenu.Item title={'博客'} url={'http://www.baidu.com'} />
+						</DropDownMenu>
 					</div>
-					<div ref="dropdownContainer" className={'dropdown topbar-left border-left'}>
-						<a href="#" className={'topbar-btn'}>
-							<span>admin@wikitec.com</span>
-							<Icon type="down" style={{marginLeft: 5}} />
-						</a>
-
-						<ul className={'dropdown-menu'}>
-							<li className={'topbar-info-btn'}> 
-								<a href="#">修改密码</a>
-							</li>
-							<li className={'topbar-info-btn'}> 
-								<a href="#">退出</a>
-							</li>
-						</ul>
+					<div className={'topbar-btn topbar-left border-left'}>
+						<DropDownMenu title={<span>admin@wikitec.com</span>}>
+							<DropDownMenu.Item title={'修改密码'} onClick={(e)=>this.handleChangePassword(e)} />
+							<DropDownMenu.Item title={'退出'} onClick={(e)=>this.handleSignOut(e)} />
+						</DropDownMenu>
 					</div>
 				</div>
 			</div>
